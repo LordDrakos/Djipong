cd5c75738c814322a2c7d0d3603d065c260a7f33
LordDrakos <drakosdna@gmail.com>
2017-02-03 12:06:30 +0000

    Initial commit

---
+++ src/Juego.java
@@ -0,0 +1,13 @@
+/**
+ * Created by adn on 27/01/17.
+ */
+import javax.swing.JFrame;
+
+public class Juego {
+    public static void main(String[]args){
+        JFrame ventana = new JFrame("Djipong");
+        ventana.setSize(400,400);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
